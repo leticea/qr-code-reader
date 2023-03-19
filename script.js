14db09064afb13559836d43af7913684cbdce5c5
leticea <nascimento.leticeah@gmail.com>
2023-03-19 11:31:26 -0300

    feat(script.js): fetch api function

---
+++ script.js
@@ -0,0 +1,14 @@
+const wrapper = document.querySelector(".wrapper");
+const form = document.querySelector("form");
+const fileInp = document.querySelector("input");
+const infoText = document.querySelector("p");
+const closeBtn = document.querySelector(".close");
+const copyBtn = document.querySelector(".copy");
+
+function fetchRequest(file, formData) {
+  infoText.innerText = "Scanning QR Code...";
+  fetch("http://api.qrserver.com/v1/read-qr-code/", {
+    method: "POST",
+    body: formData,
+  });
+}
